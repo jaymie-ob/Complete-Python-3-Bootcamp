280aa0979de6a78a982c1d8fd05b60073b24ac30
jaymie-ob <jaymie.obrien@gmail.com>
2020-04-02 18:30:06 +0100

    blackjack

---
+++ 08-Milestone Project - 2/blackjack.py
@@ -0,0 +1,18 @@
+# Blackjack.py
+import random
+
+# a card class
+class Card(object):
+    def __init__():
+        pass
+
+# a deck class
+class Deck:
+    suits = ['c', 'd', 'h', 's']
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+    def __init__():
+        pass
+
+# a hand (subset of deck)
+class Hand(Deck):
